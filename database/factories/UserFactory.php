9126f42d3269e6c0bc8001fed4f70e5a0068bf4a
tornadoghost <tornadoghost97@gmail.com>
2024-10-11 00:59:05 +0300

    removed unused classes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Models\Region;
-use Database\Seeders\RegionSeeder;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
