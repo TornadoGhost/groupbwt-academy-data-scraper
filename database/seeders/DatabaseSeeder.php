7e3ecb2223b7ee91ae5bd1fddd41e2392112763c
tornadoghost <tornadoghost97@gmail.com>
2024-10-13 21:04:47 +0300

    added ScrapedDataSeeder seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             ProductSeeder::class,
             ScrapingSessionSeeder::class,
             ScrapedDataSeeder::class,
+            ScrapedDataImageSeeder::class
         ]);
     }
 }
