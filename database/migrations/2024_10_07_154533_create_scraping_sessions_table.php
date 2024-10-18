a546a99cf0ae70ea92e893982963a7c95c01b688
tornadoghost <tornadoghost97@gmail.com>
2024-10-18 16:26:20 +0300

    updated migration

--- database/migrations/2024_10_07_154533_create_scraping_sessions_table.php
+++ database/migrations/2024_10_07_154533_create_scraping_sessions_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('scraping_sessions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('retailer_id')->constrained()->onDelete('cascade');
-            $table->foreignId('status_id')->constrained('session_statuses')->onDelete('cascade');
+            $table->foreignId('session_status_id')->constrained('session_statuses')->onDelete('cascade');
             $table->timestamp('started_at');
-            $table->timestamp('ended_at');
+            $table->timestamp('ended_at')->nullable();
         });
     }
 
