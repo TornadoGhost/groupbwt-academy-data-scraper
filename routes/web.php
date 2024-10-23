f50ea54b4ca5c1f8bc6dca8be1d898e3da5b5b9c
tornadoghost <tornadoghost97@gmail.com>
2024-10-23 04:41:29 +0300

    added main and login routes for web

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,13 @@
 <?php
 
+use App\Http\Controllers\Web\HomeController;
+use App\Http\Controllers\Web\LoginController;
+use App\Http\Middleware\CheckAuthenticated;
+use App\Http\Middleware\CheckLogged;
 use Illuminate\Support\Facades\Route;
+
+Route::middleware([CheckAuthenticated::class])->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+});
+
+Route::get('/login', [LoginController::class, 'index'])->middleware(CheckLogged::class)->name('login');
