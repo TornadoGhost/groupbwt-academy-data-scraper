508f9b13d800b428c6e7d22664ca309e722ba45a
tornadoghost <tornadoghost97@gmail.com>
2024-10-23 18:32:12 +0300

    moved generation access token to web

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,17 @@
 <?php
 
 use App\Http\Controllers\Web\HomeController;
-use App\Http\Controllers\Web\LoginController;
-use App\Http\Middleware\CheckAuthenticated;
-use App\Http\Middleware\CheckLogged;
+use App\Http\Controllers\Web\AuthController;
+use App\Http\Controllers\Web\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware([CheckAuthenticated::class])->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/products', [ProductController::class, 'index'])->name('products');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
-Route::get('/login', [LoginController::class, 'index'])->middleware(CheckLogged::class)->name('login');
+Route::controller(AuthController::class)->middleware('guest')->group(function () {
+    Route::get('/login', 'index')->name('login');
+    Route::post('/login', 'login')->name('login.store');
+});
