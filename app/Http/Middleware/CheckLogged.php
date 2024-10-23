9753eecc8baac7f6bf63926ec10efa39cfd5d5f5
tornadoghost <tornadoghost97@gmail.com>
2024-10-23 04:44:23 +0300

    added custom middleware for logged user to redirect him from login route

---
+++ app/Http/Middleware/CheckLogged.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckLogged
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $accessToken = $_COOKIE['laravel_token'] ?? null;
+
+        if (!$accessToken) {
+            return $next($request);
+        }
+
+        return redirect()->back();
+    }
+}
