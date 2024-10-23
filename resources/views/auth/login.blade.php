c8be30bc64baa771ebb482f687249b047201eed1
tornadoghost <tornadoghost97@gmail.com>
2024-10-23 04:47:33 +0300

    added blade file for login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,18 +1,19 @@
 @extends('adminlte::auth.login')
 
+@section('adminlte_css_pre')
+@stop
+
 @section('auth_header')
-        <h3 class="card-title float-none text-center">
-            Login
-        </h3>
+    <h3 class="card-title float-none text-center">
+        Login
+    </h3>
 @endsection
 
 @section('auth_body')
-    <form action="{{ config('adminlte.login_url') }}" method="post">
-        @csrf
-
+    <form id="form" class="form">
         {{-- Email field --}}
         <div class="input-group mb-3">
-            <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
+            <input type="email" name="email" class="form-control"
                    value="{{ old('email') }}" placeholder="{{ __('adminlte::adminlte.email') }}" autofocus>
 
             <div class="input-group-append">
@@ -21,11 +22,9 @@
                 </div>
             </div>
 
-            @error('email')
             <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
+                <strong id="email-error"></strong>
+            </span>
         </div>
 
         {{-- Password field --}}
@@ -39,22 +38,50 @@
                 </div>
             </div>
 
-            @error('password')
+
             <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
+                <strong id="password-error"></strong>
+            </span>
         </div>
 
         {{-- Login field --}}
         <div class="row">
             <div class="col-5">
-                <button type=submit class="btn btn-block {{ config('adminlte.classes_auth_btn', 'btn-flat btn-primary') }}">
+                <button type=submit
+                        class="btn btn-block {{ config('adminlte.classes_auth_btn', 'btn-flat btn-primary') }}">
                     <span class="fas fa-sign-in-alt"></span>
                     {{ __('adminlte::adminlte.sign_in') }}
                 </button>
             </div>
         </div>
-
     </form>
 @stop
+@push('js')
+    <script type="module">
+        import {mainFetch} from "{{ asset('js/mainFetch.js') }}";
+
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            const formData = {
+                email: form.elements.email.value,
+                password: form.elements.password.value,
+            };
+
+            mainFetch('login', 'POST', JSON.stringify(formData))
+                .then(({data}) => {
+                    if (data.errors) {
+                        const {email, password} = data.errors;
+                        form.email.classList.add("is-invalid");
+                        form.password.classList.add("is-invalid");
+                        document.getElementById('email-error').innerText = email;
+                        document.getElementById('password-error').innerText = password[0];
+                    } else {
+                        localStorage.setItem('accessToken', data.token);
+                        window.location.href = "{{ route('home') }}";
+                    }
+                })
+                .catch(error => console.log(error));
+        });
+    </script>
+@endpush
