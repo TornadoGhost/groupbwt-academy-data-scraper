cea364f18016e5de10f6ad8f14ec7dbe3c514887
tornadoghost <tornadoghost97@gmail.com>
2024-10-23 20:36:49 +0300

    changed basic url

--- resources/views/vendor/adminlte/partials/sidebar/menu-item-link.blade.php
+++ resources/views/vendor/adminlte/partials/sidebar/menu-item-link.blade.php
@@ -1,7 +1,7 @@
 <li @isset($item['id']) id="{{ $item['id'] }}" @endisset class="nav-item">
 
     <a class="nav-link {{ $item['class'] }} @isset($item['shift']) {{ $item['shift'] }} @endisset"
-       href="{{ $item['href'] }}" @isset($item['target']) target="{{ $item['target'] }}" @endisset
+       href="{{ config('url')  }}{{ $item['href'] }}"
        {!! $item['data-compiled'] ?? '' !!}>
 
         <i class="{{ $item['icon'] ?? 'far fa-fw fa-circle' }} {{
