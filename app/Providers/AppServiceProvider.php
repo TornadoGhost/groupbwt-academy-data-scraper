eb06f4bf3dc363b2d79b4bfad25ad64f2f860c5b
tornadoghost <tornadoghost97@gmail.com>
2024-10-23 18:33:23 +0300

    added access token life the same as session lifetime

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-
+        Passport::personalAccessTokensExpireIn(now()->addMinutes((int)config('session.lifetime')));
     }
 }
