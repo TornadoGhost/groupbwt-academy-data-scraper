fb309c8c0de87d7cb055d6030fc39c303f173505
tornadoghost <tornadoghost97@gmail.com>
2024-10-24 19:55:08 +0300

    added export fetch method with Auth header

--- public/js/mainFetch.js
+++ public/js/mainFetch.js
@@ -1,25 +1,19 @@
-export function mainFetch(path = '/', method = 'get', body = '', headers = {}) {
+import {getCookie} from "./getCookie.js";
+
+export function mainFetch(path, method, body, headers) {
     const token = getCookie('laravel_token');
     const options = {
         method: method,
         headers: {
             'Accept': 'application/json',
-            'Content-Type': 'application/json',
             'Authorization': `Bearer ${token}`,
             ...headers,
         },
     };
 
     if (body && (method === 'POST' || method === 'PUT' || method === 'PATCH')) {
-        options.body = JSON.stringify(body);
+        options.body = body;
     }
     return fetch(`http://localhost/api/${path}`, options)
         .then(response => response.json());
-}
-
-const allCookies = document.cookie;
-const cookiesArray = allCookies.split('; ');
-function getCookie(name) {
-    const cookie = cookiesArray.find(cookie => cookie.startsWith(`${name}=`));
-    return cookie ? cookie.split('=')[1] : null; // Повертає значення cookie або null, якщо не знайдено
 }
