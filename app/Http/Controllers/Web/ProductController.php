852ba28d0f10a70e95dbaa4ce2c9a3da28cb22d9
tornadoghost <tornadoghost97@gmail.com>
2024-10-25 13:47:07 +0300

    added getting data for view

--- app/Http/Controllers/Web/ProductController.php
+++ app/Http/Controllers/Web/ProductController.php
@@ -3,17 +3,33 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Services\Contracts\ProductServiceInterface;
+use App\Services\Contracts\RetailerServiceInterface;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+
+    public function __construct(protected ProductServiceInterface $productService, protected RetailerServiceInterface $retailerService)
+    {
+    }
+
     public function index(): View
     {
         return view('products.index');
     }
 
-    public function create()
+    public function create(): View
     {
         return view('products.create');
+    }
+
+    public function show($mpn): View
+    {
+        $product = $this->productService->find($mpn);
+        $retailers = $this->retailerService->all();
+
+        return view('products.show', compact('product', 'retailers'));
     }
 }
