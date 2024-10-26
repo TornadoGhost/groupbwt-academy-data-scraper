f6cbbffe8f111ac2cc76d9aa941ffa4dbbb29e13
tornadoghost <tornadoghost97@gmail.com>
2024-10-26 23:06:31 +0300

    added info about method type return

--- app/Http/Controllers/Web/RetailerController.php
+++ app/Http/Controllers/Web/RetailerController.php
@@ -5,15 +5,15 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use App\Services\Contracts\RetailerServiceInterface;
 use App\Services\Contracts\UserServiceInterface;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
-
 class RetailerController extends Controller
 {
     public function __construct(protected RetailerServiceInterface $retailerService, protected UserServiceInterface $userService)
     {
     }
 
-    public function index()
+    public function index(): View
     {
         $users = $this->userService->all();
         $preparedUsers = $this->userService->prepareUsers($users);
@@ -21,12 +21,12 @@ class RetailerController extends Controller
         return view('retailers.index', compact('users', 'preparedUsers'));
     }
 
-    public function create()
+    public function create(): View
     {
         return view('retailers.create');
     }
 
-    public function show(int $id)
+    public function show(int $id): View
     {
         $retailer = $this->retailerService->find($id);
 
