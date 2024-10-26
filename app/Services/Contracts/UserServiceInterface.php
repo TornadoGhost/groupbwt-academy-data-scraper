bc3b4742e90e030418b61179c559313e2204cf17
tornadoghost <tornadoghost97@gmail.com>
2024-10-26 11:16:39 +0300

    added method for preparing users data for view

--- app/Services/Contracts/UserServiceInterface.php
+++ app/Services/Contracts/UserServiceInterface.php
@@ -2,7 +2,10 @@
 
 namespace App\Services\Contracts;
 
+use Illuminate\Database\Eloquent\Collection;
+
 interface UserServiceInterface extends BaseCrudServiceInterface
 {
     public function findByEmail($email);
+    public function prepareUsers(Collection $users);
 }
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -4,11 +4,22 @@ namespace App\Services;
 
 use App\Repositories\Contracts\UserRepositoryInterface;
 use App\Services\Contracts\UserServiceInterface;
+use Illuminate\Database\Eloquent\Collection;
 
 class UserService extends BaseCrudService implements UserServiceInterface
 {
     public function findByEmail($email) {
         return $this->repository()->findByEmail($email);
+    }
+
+    public function prepareUsers(Collection $users): array
+    {
+        $preparedUsers = [];
+        foreach ($users as $user) {
+            $preparedUsers[$user->id] = "$user->username ($user->email)";
+        };
+
+        return $preparedUsers;
     }
 
     protected function getRepositoryClass(): string
