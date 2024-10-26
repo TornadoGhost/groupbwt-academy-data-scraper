6740580601011fbb2c57465867eb65a624d6cf3c
tornadoghost <tornadoghost97@gmail.com>
2024-10-26 23:12:09 +0300

    added checking for password and value

--- public/js/updatePrepareData.js
+++ public/js/updatePrepareData.js
@@ -2,6 +2,9 @@ export function updatePrepareData(form) {
     let formData = new FormData(form);
     const urlEncodedData = new URLSearchParams();
     formData.forEach((value, key) => {
+        if (key === 'password' && value === '') {
+            return;
+        }
         urlEncodedData.append(key, value);
     });
 
