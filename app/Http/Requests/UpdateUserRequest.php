65658966a64ee3b6dc3b2d1a9614be53a425a8bb
tornadoghost <tornadoghost97@gmail.com>
2024-10-26 23:08:05 +0300

    added exist check and removed required for region

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -18,7 +18,7 @@ class UpdateUserRequest extends FormRequest
             'username' => 'required|string|min:3|max:50',
             'password' => 'min:6|max:255',
             'isAdmin' => 'numeric',
-            'region_id' => 'required|numeric'
+            'region_id' => 'numeric|exists:regions,id',
         ];
     }
 }
