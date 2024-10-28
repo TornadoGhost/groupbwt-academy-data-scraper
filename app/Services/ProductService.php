bf51e3b1d367d08c36b0763fee3184a1ea4cc1c9
tornadoghost <tornadoghost97@gmail.com>
2024-10-28 10:09:02 +0200

    added method

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\Product;
 use App\Repositories\Contracts\ProductRepositoryInterface;
 use App\Services\Contracts\ProductServiceInterface;
 
@@ -10,5 +11,10 @@ class ProductService extends BaseCrudService implements ProductServiceInterface
     protected function getRepositoryClass(): string
     {
         return ProductRepositoryInterface::class;
+    }
+
+    public function findById(int $id): Product
+    {
+        return $this->repository()->findById($id);
     }
 }
