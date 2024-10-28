2d8e6b2f26ee08faaec59012aec71e1eb0e37ce7
tornadoghost <tornadoghost97@gmail.com>
2024-10-28 03:25:03 +0200

    removed method

--- app/Repositories/Contracts/ScrapedDataRepositoryInterface.php
+++ app/Repositories/Contracts/ScrapedDataRepositoryInterface.php
@@ -4,11 +4,9 @@ namespace App\Repositories\Contracts;
 
 interface ScrapedDataRepositoryInterface extends BaseRepositoryInterface
 {
-    public function avgRating(int $productId, string $mpn, int $retailerId, string $date, int $userId);
+    public function avgRating(int $productId, string $mpn, int $retailerId, string $startDate, string $endDate, int $userId);
 
-    public function avgPrice(int $productId, string $mpn, int $retailerId, string $date, int $userId);
+    public function avgPrice(int $productId, string $mpn, int $retailerId, string $startDate, string $endDate, int $userId);
 
-    public function avgImages(int $productId, string $mpn, int $retailerId, string $date, int $userId);
-
-    public function getLatestScrapedData();
+    public function avgImages(int $productId, string $mpn, int $retailerId, string $startDate, string $endDate, int $userId);
 }
