1e51dafeb3b2b40835a7fb05a8a0cf067fc563f2
tornadoghost <tornadoghost97@gmail.com>
2024-10-28 03:25:11 +0200

    removed method

--- app/Services/Contracts/ScrapedDataServiceInterface.php
+++ app/Services/Contracts/ScrapedDataServiceInterface.php
@@ -4,11 +4,9 @@ namespace App\Services\Contracts;
 
 interface ScrapedDataServiceInterface extends BaseCrudServiceInterface
 {
-    public function avgRating(int $productId, string $mpn, int $retailerId, string $date, int $userId);
+    public function avgRating(int $productId, string $mpn, int $retailerId, string $startDate, string $endDate, int $userId);
 
-    public function avgPrice(int $productId, string $mpn, int $retailerId, string $date, int $userId);
+    public function avgPrice(int $productId, string $mpn, int $retailerId, string $startDate, string $endDate, int $userId);
 
-    public function avgImages(int $productId, string $mpn, int $retailerId, string $date, int $userId);
-
-    public function getLatestScrapedData();
+    public function avgImages(int $productId, string $mpn, int $retailerId, string $startDate, string $endDate, int $userId);
 }
