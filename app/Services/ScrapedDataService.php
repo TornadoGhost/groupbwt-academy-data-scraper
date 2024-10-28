8aedb28d39a5065118f6920938d80505596b8d7b
tornadoghost <tornadoghost97@gmail.com>
2024-10-28 03:25:37 +0200

    changed date to startDate and endDate

--- app/Services/ScrapedDataService.php
+++ app/Services/ScrapedDataService.php
@@ -4,33 +4,29 @@ namespace App\Services;
 
 use App\Repositories\Contracts\ScrapedDataRepositoryInterface;
 use App\Services\Contracts\ScrapedDataServiceInterface;
+use Illuminate\Database\Eloquent\Collection;
 
 class ScrapedDataService extends BaseCrudService implements ScrapedDataServiceInterface
 {
 
 
-    protected function getRepositoryClass()
+    protected function getRepositoryClass(): string
     {
         return ScrapedDataRepositoryInterface::class;
     }
 
-    public function avgRating(int $productId = 0, string $mpn = '', int $retailerId = 0, string $date = '', int $userId = 0)
+    public function avgRating(int $productId = 0, string $mpn = '', int $retailerId = 0, string $startDate = '', string $endDate = '', int $userId = 0): Collection
     {
-        return $this->repository()->avgRating($productId, $mpn, $retailerId, $date, $userId);
+        return $this->repository()->avgRating($productId, $mpn, $retailerId, $startDate, $endDate, $userId);
     }
 
-    public function avgPrice(int $productId = 0, string $mpn = '', int $retailerId = 0, string $date = '', int $userId = 0)
+    public function avgPrice(int $productId = 0, string $mpn = '', int $retailerId = 0, string $startDate = '', string $endDate = '', int $userId = 0): Collection
     {
-        return $this->repository()->avgPrice($productId, $mpn, $retailerId, $date, $userId);
+        return $this->repository()->avgPrice($productId, $mpn, $retailerId, $startDate, $endDate, $userId);
     }
 
-    public function avgImages(int $productId = 0, string $mpn = '', int $retailerId = 0, string $date = '', int $userId = 0)
+    public function avgImages(int $productId = 0, string $mpn = '', int $retailerId = 0, string $startDate = '', string $endDate = '', int $userId = 0): Collection
     {
-        return $this->repository()->avgImages($productId, $mpn, $retailerId, $date, $userId);
-    }
-
-    public function getLatestScrapedData()
-    {
-        return $this->repository()->getLatestScrapedData();
+        return $this->repository()->avgImages($productId, $mpn, $retailerId, $startDate, $endDate, $userId);
     }
 }
