97780bb6ec193abc7dde7ed478af6383908e986f
tornadoghost <tornadoghost97@gmail.com>
2024-10-31 23:14:05 +0200

    changed default laravel pagination style from tailwind to bootstrap

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Paginator::useBootstrap();
         // Gates
         Gate::define('isAdmin', function (User $user) {
             return $user->isAdmin;
