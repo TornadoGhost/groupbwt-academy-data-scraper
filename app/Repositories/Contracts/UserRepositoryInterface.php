60edfa676a905833cc2b0087eed53203456ab2c3
tornadoghost <tornadoghost97@gmail.com>
2024-10-31 23:10:18 +0200

    added typification

--- app/Repositories/Contracts/UserRepositoryInterface.php
+++ app/Repositories/Contracts/UserRepositoryInterface.php
@@ -4,5 +4,5 @@ namespace App\Repositories\Contracts;
 
 interface UserRepositoryInterface extends BaseRepositoryInterface
 {
-    public function findByEmail($email);
+    public function findByEmail(string $email);
 }
