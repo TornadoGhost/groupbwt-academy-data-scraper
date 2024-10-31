44f4350f5df6ab503aca50926be01176d1c25f2f
tornadoghost <tornadoghost97@gmail.com>
2024-10-31 23:09:49 +0200

    changed attribute name

--- app/Repositories/Contracts/RetailerRepositoryInterface.php
+++ app/Repositories/Contracts/RetailerRepositoryInterface.php
@@ -6,7 +6,8 @@ interface RetailerRepositoryInterface extends BaseRepositoryInterface
 {
     public function grandAccess(int $retailer_id, array $users_id);
     public function revokeAccess(int $retailer_id, array $users_id);
-    public function restore(int $uid);
+    public function restore(int $id);
     public function findWithUsers(int $id);
     public function list();
+    public function retailersForMetrics(int $userId);
 }
