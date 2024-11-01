adc062c54cab4522e0f7dcd3bb567c8faf442ae6
tornadoghost <tornadoghost97@gmail.com>
2024-11-01 21:01:07 +0200

    added validation for product import file

---
+++ app/Http/Requests/ProductImportRequest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductImportRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'csv_file' => 'required|file|mimes:csv'
+        ];
+    }
+}
