99862022bdd04427e7484026ae1fc1c5672c4f74
tornadoghost <tornadoghost97@gmail.com>
2024-11-03 01:15:15 +0200

    added notification controller

---
+++ app/Http/Controllers/Api/NotificationController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Web\App;
+
+class NotificationController extends Controller
+{
+    public function show()
+    {
+//        $user = User::find(1);
+
+        /*foreach ($user->notifications as $notification) {
+             $notification->type;
+        }*/
+
+//        return response()->json($user->notifications);
+    }
+}
