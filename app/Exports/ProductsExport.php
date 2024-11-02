8f77bf74bc8be5c06bec3e7e1701d62f529ff893
tornadoghost <tornadoghost97@gmail.com>
2024-11-03 01:14:56 +0200

    added products export class

---
+++ app/Exports/ProductsExport.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Exports;
+
+
+use App\Models\Product;
+use App\Services\Contracts\ProductServiceInterface;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Throwable;
+
+class ProductsExport implements FromCollection, WithHeadings, WithColumnWidths, ShouldQueue
+{
+    use Exportable, Queueable;
+    public function __construct(
+        protected ProductServiceInterface $productService,
+    )
+    {
+    }
+
+    public function collection(): AnonymousResourceCollection|Collection
+    {
+//        $products = $this->productService->all();
+//        return ProductResource::collection($products);
+
+        return $this->productService->all()->flatMap(function ($product) {
+            return $product->retailers->map(function ($retailer) use ($product) {
+                return [
+                    'Product ID' => $product->id,
+                    'Product Name' => $product->title,
+                    'Product Manufacturer Part Number' => $product->manufacturer_part_number,
+                    'Product Pack Size' => $product->pack_size,
+                    'Product Added' => $product->created_at,
+                    'Product Url' => $retailer->pivot->product_url,
+                    'Retailer Name' => $retailer->name,
+                    'Retailer Currency' => $retailer->currency,
+                    'Retailer Reference' => $retailer->reference,
+                ];
+            });
+        });
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Product ID',
+            'Product Name',
+            'Product Manufacturer Part Number',
+            'Product Pack Size',
+            'Product Added',
+            'Product Url',
+            'Retailer Name',
+            'Retailer Currency',
+            'Retailer Reference',
+        ];
+    }
+
+    public function columnWidths(): array
+    {
+        return array(
+            'A' => 10,
+            'B' => 15,
+            'C' => 31,
+            'D' => 16,
+            'E' => 19,
+            'F' => 80,
+            'G' => 28,
+            'H' => 16,
+            'I' => 74,
+        );
+    }
+
+    /*public function failed(Throwable $exception): void
+    {
+        $this->user->notify(new NotifyUserOfFailedExport($exception->getMessage()));
+    }*/
+}
