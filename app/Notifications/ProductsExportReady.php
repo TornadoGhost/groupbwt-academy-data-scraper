7d2d5e9526d08127869a9417cadee75be0a69ede
tornadoghost <tornadoghost97@gmail.com>
2024-11-03 01:14:00 +0200

    added ready export notification

---
+++ app/Notifications/ProductsExportReady.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ProductsExportReady extends Notification
+{
+    use Queueable;
+
+    public function __construct()
+    {
+
+    }
+
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'message' => 'Products export completed.',
+        ];
+    }
+}
