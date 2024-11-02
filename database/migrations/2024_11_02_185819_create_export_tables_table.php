2b51cc59f43a9245aa2462e74b61093755314fbd
tornadoghost <tornadoghost97@gmail.com>
2024-11-03 01:16:45 +0200

    added migration for export table

---
+++ database/migrations/2024_11_02_185819_create_export_tables_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('export_tables', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_name', 50);
+            $table->string('path', 150);
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('export_tables');
+    }
+};
