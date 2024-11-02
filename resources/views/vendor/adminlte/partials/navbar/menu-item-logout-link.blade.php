d5f39d519c548fa21fd9ec625d56c9fababd6e8a
tornadoghost <tornadoghost97@gmail.com>
2024-11-03 01:18:34 +0200

    added classes

--- resources/views/vendor/adminlte/partials/navbar/menu-item-logout-link.blade.php
+++ resources/views/vendor/adminlte/partials/navbar/menu-item-logout-link.blade.php
@@ -1,5 +1,5 @@
 <li class="nav-item">
-    <form class="nav-link" action="{{route('logout')}}" method="post">
+    <form class="d-flex justify-content-center align-items-center nav-link" action="{{route('logout')}}" method="post">
         @csrf
         <x-adminlte-button id="logout" class="btn-sm" label="Logout" type="submit"/>
     </form>
