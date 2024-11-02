f74a9ec6cf40d8e359770c96334283cad828fe60
tornadoghost <tornadoghost97@gmail.com>
2024-11-03 01:13:48 +0200

    added fail export notification

---
+++ app/Notifications/NotifyUserOfFailedExport.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NotifyUserOfFailedExport extends Notification
+{
+    use Queueable;
+
+    public function __construct(protected $errors)
+    {
+        //
+    }
+
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'message' => 'The export you requested failed.',
+            'errors' => $this->errors,
+        ];
+    }
+}
