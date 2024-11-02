7ab8f9cb44d52c9aca91dc17195f6543151558b5
tornadoghost <tornadoghost97@gmail.com>
2024-11-03 01:14:28 +0200

    added notification export job

---
+++ app/Jobs/NotifyUserOfCompletedExport.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use App\Notifications\ProductsExportReady;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\SerializesModels;
+
+class NotifyUserOfCompletedExport implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public User $user)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $this->user->notify(new ProductsExportReady());
+    }
+}
