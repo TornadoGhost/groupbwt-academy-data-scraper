2f4e25f12d542a321f83fe9f622ba4a08a9be287
tornadoghost <tornadoghost97@gmail.com>
2024-11-04 02:33:49 +0200

    added data for view

--- app/Http/Controllers/Web/RetailerController.php
+++ app/Http/Controllers/Web/RetailerController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use App\Services\Contracts\RetailerServiceInterface;
+use App\Services\Contracts\ScrapingSessionServiceInterface;
 use App\Services\Contracts\UserServiceInterface;
+use Carbon\Carbon;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 class RetailerController extends Controller
 {
-    public function __construct(protected RetailerServiceInterface $retailerService, protected UserServiceInterface $userService)
+    public function __construct(
+        protected RetailerServiceInterface $retailerService,
+        protected UserServiceInterface $userService,
+        protected ScrapingSessionServiceInterface $scrapingSessionService,
+    )
     {
     }
 
@@ -17,8 +23,12 @@ class RetailerController extends Controller
     {
         $users = $this->userService->all();
         $preparedUsers = $this->userService->prepareUsers($users);
+        $firstScrapedData = $this->scrapingSessionService->getFirstScrapingSession();
+        $lastScrapedDate = $this->scrapingSessionService->getLatestScrapingSession();
+        $firstDate = Carbon::parse($firstScrapedData)->format('Y-m-d');
+        $lastDate = Carbon::parse($lastScrapedDate)->format('Y-m-d');
 
-        return view('retailers.index', compact('users', 'preparedUsers'));
+        return view('retailers.index', compact('users', 'preparedUsers', 'firstDate', 'lastDate'));
     }
 
     public function create(): View
