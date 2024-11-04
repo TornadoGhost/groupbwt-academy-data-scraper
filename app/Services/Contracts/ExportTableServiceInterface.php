c2e9afaed4fbc70a0096c2c18a337bf669338e5c
tornadoghost <tornadoghost97@gmail.com>
2024-11-04 02:41:51 +0200

    added methods

--- app/Services/Contracts/ExportTableServiceInterface.php
+++ app/Services/Contracts/ExportTableServiceInterface.php
@@ -7,4 +7,8 @@ interface ExportTableServiceInterface
     public function getExportedFiles($userId);
     public function create(string $userId, string $fileName, string $filePath);
     public function setPath(string $prefix);
+    public function show(int $id);
+    public function delete(int $id);
+    public function checkFileExistence(string $path);
+    public function getFile(string $path);
 }
