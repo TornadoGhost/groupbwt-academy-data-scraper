a38ed241c42c06216185b6da297aa578ea8c803a
tornadoghost <tornadoghost97@gmail.com>
2024-11-06 22:49:21 +0200

    removed validation rules

--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -31,32 +31,6 @@ class ProductsImport implements ToModel, WithBatchInserts, WithChunkReading, Wit
         ]);
     }
 
-    public function rules(): array
-    {
-        return [
-            'title' => ['required', 'string', 'min:3', 'max:100'],
-            'manufacturer_part_number' => [
-                'required',
-                'string',
-                'min:3',
-                'max:50',
-                Rule::unique('products')->where(fn ($query) => $query->where('user_id', auth()->id())),
-            ],
-            'pack_size' => ['required', 'string', 'min:3', 'max:20'],
-        ];
-    }
-
-    public function customValidationMessages(): array
-    {
-        return [
-            'manufacturer_part_number.required' => 'MPN field is required.',
-            'manufacturer_part_number.string' => 'MPN field should be a string.',
-            'manufacturer_part_number.min' => 'MPN field should be at least 3 characters.',
-            'manufacturer_part_number.max' => 'MPN field should be less than 50 characters.',
-            'manufacturer_part_number.unique' => 'This MPN already exists.',
-        ];
-    }
-
     public function batchSize(): int
     {
         return 100;
