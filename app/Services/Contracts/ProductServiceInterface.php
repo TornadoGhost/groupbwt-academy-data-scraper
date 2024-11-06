682a870cacc3661134a3cc89a7da51a155c0a45b
tornadoghost <tornadoghost97@gmail.com>
2024-11-06 22:50:00 +0200

    added methods

--- app/Services/Contracts/ProductServiceInterface.php
+++ app/Services/Contracts/ProductServiceInterface.php
@@ -2,11 +2,14 @@
 
 namespace App\Services\Contracts;
 
+use App\Models\User;
 use App\Services\Contracts\BaseCrudServiceInterface;
+use Illuminate\Http\UploadedFile;
 
 interface ProductServiceInterface extends BaseCrudServiceInterface
 {
     public function findByMpn(string $mpn);
-
     public function productsForMetrics(int $userId);
+    public function import(UploadedFile $file, User $user);
+    public function downloadExampleImportFile();
 }
