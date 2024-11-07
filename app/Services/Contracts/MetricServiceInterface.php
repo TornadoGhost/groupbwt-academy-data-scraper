1fb5a516d579165eb28f7c7e5d22f56a21431a1e
tornadoghost <tornadoghost97@gmail.com>
2024-11-07 03:49:05 +0200

    added method for export

--- app/Services/Contracts/MetricServiceInterface.php
+++ app/Services/Contracts/MetricServiceInterface.php
@@ -2,9 +2,12 @@
 
 namespace App\Services\Contracts;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\JsonResponse;
 
 interface MetricServiceInterface
 {
-    public function getAvgData(Collection $avgRating, array $avgPrice, array $avgImages);
+    public function getAvgData(Collection $avgRating, array $avgPrice, array $avgImages): Collection|\Illuminate\Support\Collection;
+    public function exportExcel($startDate, $endDate, User $user): JsonResponse;
 }
--- app/Services/MetricService.php
+++ app/Services/MetricService.php
@@ -2,12 +2,32 @@
 
 namespace App\Services;
 
+use App\Exports\MetricsExport;
+use App\Jobs\NotifyUserOfCompletedExport;
+use App\Jobs\SaveExportTableData;
+use App\Models\User;
+use App\Services\Contracts\ExportTableServiceInterface;
 use App\Services\Contracts\MetricServiceInterface;
+use App\Services\Contracts\ScrapedDataServiceInterface;
+use App\Services\Contracts\ScrapingSessionServiceInterface;
+use App\Traits\JsonResponseHelper;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\JsonResponse;
 
 class MetricService implements MetricServiceInterface
 {
-    public function getAvgData(Collection $avgRating,array $avgPrice,array $avgImages)
+    use JsonResponseHelper;
+
+    public function __construct(
+        protected ExportTableServiceInterface $exportTableService,
+        protected ScrapingSessionServiceInterface $scrapingSessionService,
+        protected ScrapedDataServiceInterface $scrapedDataService,
+    )
+    {
+    }
+
+    public function getAvgData(Collection $avgRating,array $avgPrice,array $avgImages): Collection|\Illuminate\Support\Collection
     {
         return $avgRating->map(function ($item) use ($avgPrice, $avgImages) {
             $retailerId = $item['retailer_id'];
@@ -20,5 +40,22 @@ class MetricService implements MetricServiceInterface
                 'average_images_per_product' => round((float)($avgImages[$retailerId]['average_images_per_product'] ?? 0), 2),
             ];
         });
+    }
+
+    public function exportExcel($startDate, $endDate, User $user): JsonResponse
+    {
+        $fileName = 'Metrics';
+        $filePath = $this->exportTableService->setPath($fileName, $user->id);
+        $startDate = $startDate ?? Carbon::parse($this->scrapingSessionService->getLatestScrapingSession())->format('Y-m-d');
+        $endDate = $endDate ?? '';
+
+        (new MetricsExport($this->scrapedDataService, $this, $startDate, $endDate))
+            ->store($filePath)
+            ->chain([
+                new NotifyUserOfCompletedExport($user, 'Metrics'),
+                new SaveExportTableData($fileName, $filePath, $user, $this->exportTableService)
+            ]);
+
+        return $this->successResponse('Metrics data export started');
     }
 }
