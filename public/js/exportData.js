da31e38834161a2edcc53cacaa16fec45312444a
tornadoghost <tornadoghost97@gmail.com>
2024-11-08 05:47:20 +0200

    changed when showing start export alert

--- public/js/exportData.js
+++ public/js/exportData.js
@@ -2,10 +2,6 @@ import {mainFetch} from "./mainFetch.js";
 import {showAlert} from "./showAlert.js"
 
 export function exportData(fetchUrl, successAlert) {
-        mainFetch(fetchUrl, 'GET')
-            .then((response) => {
-                if (response.status === "Success") {
-                    showAlert(successAlert, 'content-wrapper', true, 5000);
-                }
-            });
+    showAlert(successAlert, 'content-wrapper', true, 5000);
+    mainFetch(fetchUrl, 'GET')
 }
