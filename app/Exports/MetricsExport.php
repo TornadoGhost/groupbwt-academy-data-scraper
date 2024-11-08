eea9e163213bc804697ec8acee6608f0c4f5fdce
tornadoghost <tornadoghost97@gmail.com>
2024-11-08 05:33:25 +0200

    changed class logic from sheet to export

--- app/Exports/MetricsExport.php
+++ app/Exports/MetricsExport.php
@@ -2,19 +2,19 @@
 
 namespace App\Exports;
 
+use App\Exports\Sheets\MetricsFiltersSheet;
+use App\Exports\Sheets\MetricsSheet;
+use App\Models\User;
 use App\Services\Contracts\MetricServiceInterface;
+use App\Services\Contracts\ProductServiceInterface;
+use App\Services\Contracts\RetailerServiceInterface;
 use App\Services\Contracts\ScrapedDataServiceInterface;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\Exportable;
-use Maatwebsite\Excel\Concerns\FromCollection;
-use Maatwebsite\Excel\Concerns\WithColumnWidths;
-use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
-use Maatwebsite\Excel\Concerns\WithTitle;
 
-class MetricsExport implements FromCollection, WithHeadings, WithTitle, WithColumnWidths, ShouldQueue
+class MetricsExport implements WithMultipleSheets, ShouldQueue
 {
     use Exportable, Queueable;
 
@@ -26,71 +26,27 @@ class MetricsExport implements FromCollection, WithHeadings, WithTitle, WithColu
         protected string                      $startDate,
         protected string                      $endDate,
         protected int                         $userId,
+        protected array                       $filters,
+        protected RetailerServiceInterface    $retailerService,
+        protected ProductServiceInterface     $productService,
+        protected User                        $user,
     )
     {
     }
 
-    public function collection(): Collection
-    {
-        $avgRating = $this->scrapedDataService->avgRating(
-            $this->products,
-            $this->retailers,
-            $this->startDate,
-            $this->endDate,
-            $this->userId,
-        );
-        $avgPrice = $this->scrapedDataService->avgPrice(
-            $this->products,
-            $this->retailers,
-            $this->startDate,
-            $this->endDate,
-            $this->userId,
-        );
-        $avgImages = $this->scrapedDataService->avgImages(
-            $this->products,
-            $this->retailers,
-            $this->startDate,
-            $this->endDate,
-            $this->userId,
-        );
-
-        $avgPriceMap = $avgPrice->keyBy('retailer_id')->toArray();
-        $avgImagesMap = $avgImages->keyBy('retailer_id')->toArray();
-
-        $mergedData = collect($this->metricService->getAvgData($avgRating, $avgPriceMap, $avgImagesMap));
-
-        return $mergedData->map(function ($metric) {
-            return [
-                'Retailer name' => $metric['retailer_name'],
-                'Average product rating' => $metric['average_product_rating'],
-                'Average product price' => $metric['average_product_price'],
-                'Average images per product' => $metric['average_images_per_product'],
-            ];
-        });
-    }
-
-    public function headings(): array
+    public function sheets(): array
     {
         return [
-            'Retailer name',
-            'Average product rating',
-            'Average product price',
-            'Average images per product',
+            new MetricsFiltersSheet($this->filters, $this->retailerService, $this->productService, $this->user),
+            new MetricsSheet(
+                $this->scrapedDataService,
+                $this->metricService,
+                $this->products,
+                $this->retailers,
+                $this->startDate,
+                $this->endDate,
+                $this->userId,
+            ),
         ];
-    }
-
-    public function columnWidths(): array
-    {
-        return array(
-            'A' => 28,
-            'B' => 20,
-            'C' => 20,
-            'D' => 25,
-        );
-    }
-
-    public function title(): string
-    {
-        return 'Metrics';
     }
 }
