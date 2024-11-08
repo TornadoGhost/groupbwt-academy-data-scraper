27b3c691c8e4f19cabbe1db13670b5e724883dc0
tornadoghost <tornadoghost97@gmail.com>
2024-11-08 05:06:07 +0200

    added all filters for export metrics

--- app/Services/MetricService.php
+++ app/Services/MetricService.php
@@ -2,12 +2,14 @@
 
 namespace App\Services;
 
-use App\Exports\MetricsExport;
+use App\Exports\Sheets\MetricsWithFiltersSheet;
 use App\Jobs\NotifyUserOfCompletedExport;
 use App\Jobs\SaveExportTableData;
 use App\Models\User;
 use App\Services\Contracts\ExportTableServiceInterface;
 use App\Services\Contracts\MetricServiceInterface;
+use App\Services\Contracts\ProductServiceInterface;
+use App\Services\Contracts\RetailerServiceInterface;
 use App\Services\Contracts\ScrapedDataServiceInterface;
 use App\Services\Contracts\ScrapingSessionServiceInterface;
 use App\Traits\JsonResponseHelper;
@@ -20,14 +22,16 @@ class MetricService implements MetricServiceInterface
     use JsonResponseHelper;
 
     public function __construct(
-        protected ExportTableServiceInterface $exportTableService,
+        protected ExportTableServiceInterface     $exportTableService,
         protected ScrapingSessionServiceInterface $scrapingSessionService,
-        protected ScrapedDataServiceInterface $scrapedDataService,
+        protected ScrapedDataServiceInterface     $scrapedDataService,
+        protected RetailerServiceInterface        $retailerService,
+        protected ProductServiceInterface         $productService,
     )
     {
     }
 
-    public function getAvgData(Collection $avgRating,array $avgPrice,array $avgImages): Collection|\Illuminate\Support\Collection
+    public function getAvgData(Collection $avgRating, array $avgPrice, array $avgImages): Collection|\Illuminate\Support\Collection
     {
         return $avgRating->map(function ($item) use ($avgPrice, $avgImages) {
             $retailerId = $item['retailer_id'];
@@ -42,14 +46,30 @@ class MetricService implements MetricServiceInterface
         });
     }
 
-    public function exportExcel($startDate, $endDate, User $user): JsonResponse
+    public function exportExcel(array $requestData, User $user): JsonResponse
     {
         $fileName = 'Metrics';
         $filePath = $this->exportTableService->setPath($fileName, $user->id);
-        $startDate = $startDate ?? Carbon::parse($this->scrapingSessionService->getLatestScrapingSession())->format('Y-m-d');
-        $endDate = $endDate ?? '';
+        $products = $requestData['products'] ?? [];
+        $retailers = $requestData['retailers'] ?? [];
+        $startDate = $requestData['start_date'] ?? Carbon::parse($this->scrapingSessionService->getLatestScrapingSession())
+            ->format('Y-m-d');
+        $endDate = $requestData['end_date'] ?? '';
+        $userId = $requestData['user_id'] ?? 0;
 
-        (new MetricsExport($this->scrapedDataService, $this, $startDate, $endDate))
+        (new MetricsWithFiltersSheet(
+            $this->scrapedDataService,
+            $this,
+            $products,
+            $retailers,
+            $startDate,
+            $endDate,
+            $userId,
+            $requestData,
+            $this->retailerService,
+            $this->productService,
+            request()->user(),
+        ))
             ->store($filePath)
             ->chain([
                 new NotifyUserOfCompletedExport($user, 'Metrics'),
