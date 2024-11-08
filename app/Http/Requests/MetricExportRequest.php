6a25bd6300198765a28945c772698a06b077a726
tornadoghost <tornadoghost97@gmail.com>
2024-11-08 05:01:18 +0200

    added validation rules

--- app/Http/Requests/MetricExportRequest.php
+++ app/Http/Requests/MetricExportRequest.php
@@ -9,8 +9,13 @@ class MetricExportRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'products' => ['nullable', 'array'],
+            'products.*' => ['integer', 'exists:products,id'],
+            'retailers' => ['nullable', 'array'],
+            'retailers.*' => ['integer', 'exists:retailers,id'],
             'start_date' => ['nullable', 'date_format:Y-m-d'],
             'end_date' => ['nullable', 'date_format:Y-m-d'],
+            'user_id' => ['nullable', 'integer', 'exists:users,id'],
         ];
     }
 }
