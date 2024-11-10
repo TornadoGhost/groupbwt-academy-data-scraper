ee58fd56aa3a5ad46c8c482e1a2f9bcbee9e0efa
tornadoghost <tornadoghost97@gmail.com>
2024-11-11 00:05:34 +0200

    added getNotification method

--- app/Services/Contracts/NotificationServiceInterface.php
+++ app/Services/Contracts/NotificationServiceInterface.php
@@ -2,7 +2,11 @@
 
 namespace App\Services\Contracts;
 
+use App\Models\User;
+use Illuminate\Contracts\Auth\Authenticatable;
+
 interface NotificationServiceInterface
 {
-    public function getRightTime(string $time);
+    public function getRightTime(string $time): string;
+    public function getNotification(User|Authenticatable $user): array;
 }
--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -2,11 +2,42 @@
 
 namespace App\Services;
 
+use App\Models\User;
 use App\Services\Contracts\NotificationServiceInterface;
 use Carbon\Carbon;
+use Illuminate\Contracts\Auth\Authenticatable;
 
 class NotificationService implements NotificationServiceInterface
 {
+    public function getNotification(User|Authenticatable $user): array
+    {
+        $notifications = $user->notifications;
+        $unreadNotifications = $user->unreadNotifications;
+        $dropdownHtml = '';
+
+        foreach ($unreadNotifications as $key => $not) {
+            $icon = "<i class='mr-2 fas fa-fw fa-file text-success'></i>";
+
+            $time = "<span class='float-right text-muted text-sm'>"
+                . $this->getRightTime($not->created_at) .
+                "</span>";
+
+            $dropdownHtml .= "<a href=" . route('exportTables.index') . " class='dropdown-item'>
+                                {$icon}{$not['data']['message']}{$time}
+                              </a>";
+
+            if ($key < count($notifications) - 1) {
+                $dropdownHtml .= "<div class='dropdown-divider'></div>";
+            }
+        }
+
+        return [
+            'label' => count($unreadNotifications),
+            'label_color' => 'danger',
+            'icon_color' => 'dark',
+            'dropdown' => $dropdownHtml,
+        ];
+    }
     public function getRightTime(string $time): string
     {
         $creationDate = Carbon::parse($time);
