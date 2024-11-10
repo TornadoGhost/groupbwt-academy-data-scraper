53a909752da1cc65cc3a945b7c8ac46b7c2b2607
tornadoghost <tornadoghost97@gmail.com>
2024-11-10 03:08:35 +0200

    added return type

--- app/Services/Contracts/ProductServiceInterface.php
+++ app/Services/Contracts/ProductServiceInterface.php
@@ -2,17 +2,20 @@
 
 namespace App\Services\Contracts;
 
+use App\Models\Product;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\UploadedFile;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 interface ProductServiceInterface extends BaseCrudServiceInterface
 {
-    public function findByMpn(string $mpn);
-    public function productsForMetrics(int $userId);
-    public function import(UploadedFile $file, User $user);
-    public function downloadExampleImportFile();
-    public function exportExcel(User $user);
+    public function findByMpn(string $mpn): Product;
+    public function productsForMetrics(int $userId): Collection;
+    public function import(UploadedFile $file, User $user): JsonResponse;
+    public function downloadExampleImportFile(): StreamedResponse;
+    public function exportExcel(User $user): JsonResponse;
     public function getNameById(int $id): string;
     public function allLatest(User $user): Collection;
 }
