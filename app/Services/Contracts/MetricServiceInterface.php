a4bf226482f74f60ae586831451234d3070eeb81
tornadoghost <tornadoghost97@gmail.com>
2024-11-11 00:05:06 +0200

    added getMetrics method

--- app/Services/Contracts/MetricServiceInterface.php
+++ app/Services/Contracts/MetricServiceInterface.php
@@ -2,7 +2,9 @@
 
 namespace App\Services\Contracts;
 
+use App\Http\Requests\MetricRequest;
 use App\Models\User;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
 
@@ -10,4 +12,5 @@ interface MetricServiceInterface
 {
     public function getAvgData(Collection $avgRating, array $avgPrice, array $avgImages): Collection|\Illuminate\Support\Collection;
     public function exportExcel(array $requestData, User $user): JsonResponse;
+    public function getMetrics(MetricRequest $request, User|Authenticatable $user): Collection|\Illuminate\Support\Collection;
 }
