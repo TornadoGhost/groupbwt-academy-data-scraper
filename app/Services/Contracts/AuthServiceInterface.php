f5d6fc53a0067233d9e45f2db3f678ac5cb33074
tornadoghost <tornadoghost97@gmail.com>
2024-11-11 03:15:54 +0200

    added methods for web login and logout

--- app/Services/Contracts/AuthServiceInterface.php
+++ app/Services/Contracts/AuthServiceInterface.php
@@ -2,10 +2,15 @@
 
 namespace App\Services\Contracts;
 
+use App\Http\Requests\StoreAuthenticateRequest;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 
 interface AuthServiceInterface
 {
-    public function login($request): JsonResponse;
-    public function logout(): JsonResponse;
+    public function loginApi($request): JsonResponse;
+    public function logoutApi(): JsonResponse;
+    public function loginWeb(StoreAuthenticateRequest $request): RedirectResponse;
+    public function logoutWeb(Request $request): RedirectResponse;
 }
