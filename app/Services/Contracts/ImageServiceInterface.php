752a3850fc261b1f3d7badcf67da519e97d1440e
tornadoghost <tornadoghost97@gmail.com>
2024-11-11 06:03:40 +0200

    changed method name

--- app/Services/Contracts/ImageServiceInterface.php
+++ app/Services/Contracts/ImageServiceInterface.php
@@ -3,11 +3,12 @@
 namespace App\Services\Contracts;
 
 use App\Http\Requests\ImageProductRequest;
+use App\Models\Product;
 
 interface ImageServiceInterface
 {
     public function saveImage(string $path): false|string;
     public function deleteImageByPath(string $path): bool;
     public function deleteImageById(int $id): null|string;
-    public function store(ImageProductRequest $request): array;
+    public function storeImage(ImageProductRequest $request, Product $product): array;
 }
