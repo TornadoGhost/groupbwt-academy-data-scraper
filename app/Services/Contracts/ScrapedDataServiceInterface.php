dd1087f243a9c6d875264c0b37c4ec2cab16914f
tornadoghost <tornadoghost97@gmail.com>
2024-11-11 02:56:51 +0200

    added return type

--- app/Services/Contracts/ScrapedDataServiceInterface.php
+++ app/Services/Contracts/ScrapedDataServiceInterface.php
@@ -3,15 +3,24 @@
 namespace App\Services\Contracts;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
 
 interface ScrapedDataServiceInterface extends BaseCrudServiceInterface
 {
-    public function avgRating(array $products, array $retailers, string $startDate, string $endDate, int $userId);
+    public function avgRating(
+        array $products, array $retailers, string $startDate, string $endDate, int $userId
+    ): Collection;
 
-    public function avgPrice(array $products, array $retailers, string $startDate, string $endDate, int $userId);
+    public function avgPrice(
+        array $products, array $retailers, string $startDate, string $endDate, int $userId
+    ): Collection;
 
-    public function avgImages(array $products, array $retailers, string $startDate, string $endDate, int $userId);
+    public function avgImages(
+        array $products, array $retailers, string $startDate, string $endDate, int $userId
+    ): Collection;
+
+    public function scrapedDataByRetailer(int $retailerId, string $date): ?Collection;
 
     public function exportByRetailer(int $retailer_id, string $date, User $user, array $filters): JsonResponse;
 }
