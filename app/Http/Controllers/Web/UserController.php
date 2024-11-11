37eb89354f993d3db8e4f1b741d962c3e4af6325
tornadoghost <tornadoghost97@gmail.com>
2024-11-11 03:18:13 +0200

    added return type

--- app/Http/Controllers/Web/UserController.php
+++ app/Http/Controllers/Web/UserController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use App\Services\Contracts\RegionServiceInterface;
 use App\Services\Contracts\UserServiceInterface;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\View\View;
 
 class UserController extends Controller
 {
-    public function __construct(protected RegionServiceInterface $regionService, protected UserServiceInterface $userService)
+    public function __construct(
+        protected RegionServiceInterface $regionService,
+        protected UserServiceInterface   $userService
+    )
     {
     }
 
@@ -19,14 +24,14 @@ class UserController extends Controller
         return view('users.index');
     }
 
-    public function create()
+    public function create(): View
     {
         $regions = $this->regionService->all();
 
         return view('users.create', compact('regions'));
     }
 
-    public function show(int $id)
+    public function show(int $id): View
     {
         $user = $this->userService->find($id)->toArray();
         $regions = $this->regionService->all();
