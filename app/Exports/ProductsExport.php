9aa0cdf1d05d76172fde291268a71674de0b4f64
tornadoghost <tornadoghost97@gmail.com>
2024-11-11 02:53:55 +0200

    changed to export products with filters

--- app/Exports/ProductsExport.php
+++ app/Exports/ProductsExport.php
@@ -2,7 +2,13 @@
 
 namespace App\Exports;
 
+use App\Exports\Sheets\ProductsFiltersSheet;
+use App\Exports\Sheets\ProductsSheet;
+use App\Exports\Sheets\ScrapedDataByRetailerFiltersSheet;
+use App\Exports\Sheets\ScrapedDataByRetailerSheet;
 use App\Services\Contracts\ProductServiceInterface;
+use App\Services\Contracts\RetailerServiceInterface;
+use App\Services\Contracts\ScrapedDataServiceInterface;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
@@ -11,63 +17,24 @@ use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithColumnWidths;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
-class ProductsExport implements FromCollection, WithHeadings, WithColumnWidths, ShouldQueue
+class ProductsExport implements WithMultipleSheets, ShouldQueue
 {
     use Exportable, Queueable;
 
     public function __construct(
+        protected array                   $filters,
         protected ProductServiceInterface $productService,
     )
     {
     }
 
-    public function collection(): AnonymousResourceCollection|Collection
-    {
-        return $this->productService->all()->flatMap(function ($product) {
-            return $product->retailers->map(function ($retailer) use ($product) {
-                return [
-                    'Product ID' => $product->id,
-                    'Product Name' => $product->title,
-                    'Product Manufacturer Part Number' => $product->manufacturer_part_number,
-                    'Product Pack Size' => $product->pack_size,
-                    'Product Added' => $product->created_at,
-                    'Product Url' => $retailer->pivot->product_url,
-                    'Retailer Name' => $retailer->name,
-                    'Retailer Currency' => $retailer->currency,
-                    'Retailer Reference' => $retailer->reference,
-                ];
-            });
-        });
-    }
-
-    public function headings(): array
+    public function sheets(): array
     {
         return [
-            'Product ID',
-            'Product Name',
-            'Product Manufacturer Part Number',
-            'Product Pack Size',
-            'Product Added',
-            'Product Url',
-            'Retailer Name',
-            'Retailer Currency',
-            'Retailer Reference',
+            new ProductsFiltersSheet($this->filters),
+            new ProductsSheet($this->productService),
         ];
-    }
-
-    public function columnWidths(): array
-    {
-        return array(
-            'A' => 10,
-            'B' => 15,
-            'C' => 31,
-            'D' => 16,
-            'E' => 19,
-            'F' => 80,
-            'G' => 28,
-            'H' => 16,
-            'I' => 74,
-        );
     }
 }
